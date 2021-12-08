fec8d919011e778c84babb428bd025274a5d099d
alexeymoskvinov <alexey.moskvinov@yandex.ru>
2021-12-08 18:52:19 +0300

    start time some fix

--- startTime.js
+++ startTime.js
@@ -1,6 +1,5 @@
-var startTime = Date.now();
 window.onload = function() {
-    let speed = startTime - performance.now()
+    let speed = performance.now()
     var tag = document.createElement("h3");
     var text = document.createTextNode(`Скорость загрузки - ${speed} мс`);
     tag.appendChild(text);
